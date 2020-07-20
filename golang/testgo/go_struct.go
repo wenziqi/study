7b81af5008170b05e8fe77d21d69707e3933eb5d
youzhen <952623276@qq.com>
2020-07-19 23:31:54 -0700

    modify

--- golang/testgo/go_struct.go
+++ golang/testgo/go_struct.go
@@ -15,25 +15,25 @@ func getName(st *Student) string {
 func main() {
 
 	var s = new(Student)
-	s.name = "wenshaoqi"
+	s.name = "Youzhen"
 	s.age = 30
 	s.sex = 1
 	fmt.Println(s)
 
 	var ss = &Student{}
-	ss.name = "wenshaoqi"
+	ss.name = "Jacky"
 	ss.age = 30
 	ss.sex = 1
 	fmt.Println(ss)
 
-	st := &Student{name:"Wenshaoqi",age:20}
+	st := &Student{name:"Lily",age:20}
 	fmt.Println(st)
 
 	fmt.Println(getName(s))
 	fmt.Println(getName(st))
 
 	var s2 Student
-	s2.name = "YouZshen"
+	s2.name = "Tom"
 	fmt.Println(getName(&s2))
 	
 
